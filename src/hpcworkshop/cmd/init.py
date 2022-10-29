0bbc807c1614835791668e6bb9177c7bb77981d2
NucciTheBoss <nucci.programming@gmail.com>
2022-10-29 23:24:57 +0200

    Get general structure for CLI setup

---
+++ src/hpcworkshop/cmd/init.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jason C. Nucciarone
+# See LICENSE file for licensing details.
---
+++ src/hpcworkshop/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jason C. Nucciarone
+# See LICENSE file for licensing details.
+
+"""hpcworkshop program for the quick setup of an HPC cluster with LXD."""
+
+from __future__ import annotations
+
+import argparse
+
+
+def main() -> None:
+    print("This is a test for setup.py")
+    # parser = argparse.ArgumentParser()
+
