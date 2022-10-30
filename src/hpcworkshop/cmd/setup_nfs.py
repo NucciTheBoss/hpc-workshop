cc9a17108f3e1a585410f9704732425563ab2187
NucciTheBoss <nucci.programming@gmail.com>
2022-10-30 18:08:16 +0100

    Scaffold setup helper commands

---
+++ src/hpcworkshop/cmd/setup_nfs.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jason C. Nucciarone
+# See LICENSE file for licensing details.
+
+"""Setup NFS command for setting up NFS inside the micro-HPC cluster."""
+
+from __future__ import annotations
